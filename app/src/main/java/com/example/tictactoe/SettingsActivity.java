26f201d424391d2fc2c62edad7fba79e53fdaf07
Andrea Aceto <59835522+andreaceto@users.noreply.github.com>
2023-02-02 12:26:24 +0100

    settings page basic impl

--- app/src/main/java/com/example/tictactoe/SettingsActivity.java
+++ app/src/main/java/com/example/tictactoe/SettingsActivity.java
@@ -1,8 +1,10 @@
 package com.example.tictactoe;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -10,5 +12,16 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+    }
+
+    public void switchTheme(View v){
+        if(AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES)
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        else
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+    }
+
+    public void editPlayers(View v){
+
     }
 }
