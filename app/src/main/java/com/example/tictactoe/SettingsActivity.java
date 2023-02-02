73519fae0fc4194c5ad41075c54f1ccc607af5e1
Andrea Aceto <59835522+andreaceto@users.noreply.github.com>
2023-02-02 15:24:57 +0100

    Pop-up Dialog Box for editing players name

--- app/src/main/java/com/example/tictactoe/SettingsActivity.java
+++ app/src/main/java/com/example/tictactoe/SettingsActivity.java
@@ -1,12 +1,22 @@
 package com.example.tictactoe;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class SettingsActivity extends AppCompatActivity {
+
+    private AlertDialog.Builder dialogBuilder;
+    private AlertDialog dialog;
+
+    //Pop-up views
+    private EditText editTxtP1, editTxtP2;
+    private Button saveBtn, cancelBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +32,21 @@ public class SettingsActivity extends AppCompatActivity {
     }
 
     public void editPlayers(View v){
+        createEditPlayersDialog();
+    }
 
+    public void createEditPlayersDialog(){
+        dialogBuilder = new AlertDialog.Builder(this);
+        final View editPlayersPopUp = getLayoutInflater().inflate(R.layout.edit_players_popup,null);
+
+        editTxtP1 = editPlayersPopUp.findViewById(R.id.editTxtP1);
+        editTxtP2 = editPlayersPopUp.findViewById(R.id.editTxtP2);
+
+        saveBtn = editPlayersPopUp.findViewById(R.id.saveBtn);
+        cancelBtn = editPlayersPopUp.findViewById(R.id.cancelBtn);
+
+        dialogBuilder.setView(editPlayersPopUp);
+        dialog = dialogBuilder.create();
+        dialog.show();
     }
 }
